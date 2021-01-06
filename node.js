1116e6d61bc0669ad2720f47d6f60d6505b86319
genesismandela <mandelasamboy@gmail.com>
2021-01-06 17:31:43 +0000

    Set up Node.js environment
    1. Prepare the node enviroment to make a dynamic website with request and responses.
    
    2. Require three core modules:
    - fs
    - http
    -url
    
    3. Store html templates in variables using readFileSync
    
    4. Read the json file with the product data and transform it to an object to work with it later on.
    
    5. Create the server with a small routing and API that changes the url with different request
    
    6 Jan 2021

---
+++ node.js
@@ -0,0 +1,58 @@
+// Modules
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// Templates HTML
+const tempDescription = fs.readFileSync(
+	`${__dirname}/templates/template-description.html`,
+	"utf-8",
+);
+const tempLandpage = fs.readFileSync(
+	`${__dirname}/templates/landpage.html`,
+	"utf-8",
+);
+const tempProducts = fs.readFileSync(
+	`${__dirname}/templates/template-products.html`,
+	"utf-8",
+);
+const tempIndex = fs.readFileSync(`${__dirname}/templates/index.html`, "utf-8");
+
+// JSON Data
+const data = fs.readFileSync(`${__dirname}/products.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+// Server
+const server = http.createServer((req, res) => {
+	// Get the URL pathname
+	const { query, pathname } = url.parse(req.url, true);
+	console.log(url.parse(req.url, true));
+
+	// Store URL in variable
+	const pathName = req.url;
+
+	// Routing that link different websites depend on the req
+	if (pathname === "/" || pathname === "/landpage") {
+		res.writeHead(200, { "Content-type": "text/html" });
+
+		res.end(tempIndex);
+	} else if (pathname === "/products") {
+		res.writeHead(200, { "Content-type": "text/html" });
+
+		res.end("tempProducts");
+	} else if (pathname === "/description") {
+		res.writeHead(200, { "Content-type": "text/html" });
+
+		res.end("tempDesc");
+	} else {
+		res.writeHead(404, {
+			"Content-type": "text/html",
+		});
+
+		res.end("<h1> 404 ERROR, Page not found!</h1>");
+	}
+});
+
+server.listen(3000, "127.0.0.1", () => {
+	console.log("Listening requests: port 3000");
+});
